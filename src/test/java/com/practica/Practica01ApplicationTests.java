f4f2b7eb79e63d77639f9614740349bf14cd60c4
zeldxh <783613+zeldxh@users.noreply.github.com>
2026-09-22 06:57:23 -0600

    test(app): add Spring Boot application context load test

---
+++ src/test/java/com/practica/Practica01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
